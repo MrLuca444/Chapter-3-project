cdef2afbfe665800e2a10e1f8536047a1822a4f0
MrLuca444 <cog2808@email.vccs.edu>
2025-03-07 14:30:10 +0000

    //

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 
 int main() { 
 
-  int principal;
-  int rate ;
-  int time ;
-  int parenthesis = rate / time + 1 ;
-  int powwow = pow(parenthesis, time);
+  extern int principal;
+  extern int rate ;
+  extern int length ;
+  int parenthesis = rate / length + 1 ;
+  int powwow = pow(parenthesis, length);
   int amount = principal * powwow; 
   std::cout << "Enter the interest rate, times compounded, and the principle: ";
-  std::cin >> rate >> time >> principal;
+  std::cin >> rate >> length >> principal;
   std::cout << "Interest Rate: " << rate << std::endl;
-  std::cout << "Times Compounded: " << time << std::endl;
+  std::cout << "Times Compounded: " << length << std::endl;
   std::cout << "Principle: " << principal << std::endl;
   std::cout << "Amount in Savings: " << amount << std::endl;
   return 0;
